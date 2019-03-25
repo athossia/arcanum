80847110b03933ac95068768a9748bb3e17d4b57
athossia <motorola.for.president@gmail.com>
2019-03-25 19:05:13 +0900

    Updates

---
+++ precache-manifest.c5be4bb084247f606a112dde675677ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "30652305ca828b4cdb79936fd0398ccb",
+    "url": "/arcanum/static/media/parchment_bg.30652305.jpg"
+  },
+  {
+    "revision": "c5d9609b9a33bff8b5b193731d789d70",
+    "url": "/arcanum/static/media/LibreBaskerville-Regular.c5d9609b.otf"
+  },
+  {
+    "revision": "3e9fc8970ccf8c277acf",
+    "url": "/arcanum/static/js/runtime~main.6775b79d.js"
+  },
+  {
+    "revision": "85134b1bc21d62b4dcdd",
+    "url": "/arcanum/static/js/main.cc1d5af7.chunk.js"
+  },
+  {
+    "revision": "4eec0762c0b02bb05b4f",
+    "url": "/arcanum/static/js/2.538ed0c3.chunk.js"
+  },
+  {
+    "revision": "85134b1bc21d62b4dcdd",
+    "url": "/arcanum/static/css/main.880b7c68.chunk.css"
+  },
+  {
+    "revision": "350d7aebd80341d95c16a2f9337f2a78",
+    "url": "/arcanum/index.html"
+  }
+];
